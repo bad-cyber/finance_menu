0b1bd741906bec3244e61421caf4a44e6df5164a
Никита <rtyuiop20032w2w@gmail.com>
2023-09-04 21:09:09 +0300

    1

--- finance_menu/Form1.vb
+++ finance_menu/Form1.vb
@@ -11,19 +11,27 @@ Public Class Form1
     End Sub
 
     Private Sub Aa1_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        ProgressBar1.Value += TextBox1.Text 'Изменение значения у progressbar'а
-        Label1.Text = ProgressBar1.Maximum 'Принятие данных надписью "MONEY"
-        My.Settings.xZeyZSDtZ_M8PpJCn = ProgressBar1.Maximum 'Присвоение показателя в фиксированную настройку
-        My.Settings.Upgrade() 'Дубль сохранения данных
-        My.Settings.Save() 'Дубль сохранения данных
+        If TextBox1.Text = "" Then
+            MsgBox("Внимание! Пустые строки не являются переменными! Введите необходимые данные и повторите попытку.", MsgBoxStyle.Critical, "Ошибка №63!") : Exit Sub
+        Else
+            ProgressBar1.Value += TextBox1.Text 'Изменение значения у progressbar'а
+            Label1.Text = ProgressBar1.Maximum 'Принятие данных надписью "MONEY"
+            My.Settings.xZeyZSDtZ_M8PpJCn = ProgressBar1.Maximum 'Присвоение показателя в фиксированную настройку
+            My.Settings.Upgrade() 'Дубль сохранения данных
+            My.Settings.Save() 'Дубль сохранения данных
+        End If
     End Sub
 
     Private Sub Bb2_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        ProgressBar1.Maximum += TextBox1.Text 'Изменение значения у progressbar'а
-        Label1.Text = ProgressBar1.Maximum 'Принятие данных надписью "MONEY"
-        My.Settings.xZeyZSDtZ_M8PpJCn = ProgressBar1.Maximum 'Присвоение показателя в фиксированную настройку
-        My.Settings.Upgrade() 'Дубль сохранения данных
-        My.Settings.Save() 'Дубль сохранения данных
+        If TextBox1.Text = "" Then
+            MsgBox("Внимание! Пустые строки не являются переменными! Введите необходимые данные и повторите попытку.", MsgBoxStyle.Critical, "Ошибка №63!") : Exit Sub
+        Else
+            ProgressBar1.Maximum += TextBox1.Text 'Изменение значения у progressbar'а
+            Label1.Text = ProgressBar1.Maximum 'Принятие данных надписью "MONEY"
+            My.Settings.xZeyZSDtZ_M8PpJCn = ProgressBar1.Maximum 'Присвоение показателя в фиксированную настройку
+            My.Settings.Upgrade() 'Дубль сохранения данных
+            My.Settings.Save() 'Дубль сохранения данных
+        End If
     End Sub
     Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
         My.Settings.xZeyZSDtZ_M8PpJCn = ProgressBar1.Maximum - ProgressBar1.Value 'Сохраняем результаты изменений в приложение
